7ea922a8ccd33703bd71afa4366142d34984b012
rrvc12 <rruilova12@gmail.com>
2022-05-10 22:27:12 -0500

    Se sube el proyecto con diseño parcial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='logo-contenedor'>
+        <h1>Manejador de Tareas</h1>
+      </div>
+      <div className='tareas-lista-principal'>
+        <div>Mis tareas:</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
