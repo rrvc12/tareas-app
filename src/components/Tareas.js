5d9f7fbbc36e764d872daab92682d43a660f4776
rrvc12 <rruilova12@gmail.com>
2022-05-13 22:52:08 -0500

    Se añade el boton de editar y diseño parcial

--- src/components/Tareas.js
+++ src/components/Tareas.js
@@ -1,19 +1,49 @@
 import React from "react";
 import '../styles/Tareas.css'
-import { AiFillCloseSquare } from "react-icons/ai";
+import { AiFillCloseSquare, AiFillEdit } from "react-icons/ai";
+import { useState } from "react";
+
 
 function Tareas({ id, texto, completada, completarTarea, eliminarTarea }){
+
+    const [isEdit, setIsEdit] = useState(false);
+
+
+
+
     return (
-        <div className={completada? 'tarea-contenedor completada' : 'tarea-contenedor'}>
-            <div className='tarea-texto'
-                onClick={()=> completarTarea(id)}>
-                {texto}
-            </div>
-            <div className="tarea-contenedor-icono"
-                onClick={()=> eliminarTarea(id)}>
-                <AiFillCloseSquare className="tarea-icono" />
-            </div>
-        </div>
+        <>
+            {isEdit ? 
+                <form className='tarea-contenedor-editar'>
+                    <input 
+                    className='tarea-editar-input' 
+                    type='text' 
+                    value={texto}
+                    name='texto'/>
+                    <button className='tarea-guardar-boton'>Guardar</button>
+                </form>
+                
+                
+                :
+                <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
+                    <div className='tarea-texto'
+                        onClick={() => completarTarea(id)}>
+                        {texto}
+                    </div>
+                    <div className='tarea-contenedor-icono'>
+                        <div className='icono-editar' onClick={() => setIsEdit(true)}>
+                            <AiFillEdit className='tarea-icono' />
+                        </div>
+                        <div className='icono-eliminar' onClick={() => eliminarTarea(id)}>
+                            <AiFillCloseSquare className='tarea-icono' />
+                        </div>
+
+                    </div>
+                </div>
+                }
+
+        </>
+
     );
 }
 
